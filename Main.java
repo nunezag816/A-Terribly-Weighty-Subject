75b036dd282418d78a059099861cec2a7a74baef
nunezag816 <nunezag@msoe.edu>
2025-06-08 21:57:45 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Ounce Conversion Program!\n");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("How many ounces do you have? ");
+        int ounces = scanner.nextInt();
+
+        System.out.println("Thank you! Converting ounces to pounds.\n");
+
+        System.out.println(Converter.toPoundsAndOunces(ounces));
+        System.out.println(Converter.toDecimalPounds(ounces));
+
+        System.out.println("\nThank you for using the OCP!");
+        scanner.close();
+    }
+}
